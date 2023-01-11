d73a8a4c724cf6a54f023458278d595cc94921d8
Unai <unai.trabajo@hotmail.com>
2023-01-11 13:34:14 +0100

    JavaScript(BOM&DOM)

---
+++ 03-JavaScript/03-BOM/time/script.js
@@ -0,0 +1,16 @@
+//Cuadros de dialogo
+
+const nombre = prompt("Insertar nombre");
+alert("Hola: " + nombre);
+const asistir = confirm("Asistirás¿?");
+asistir ? alert("Si asistirás") : alert("No asistirás");
+
+//Eventos de Tiempo
+
+// const saludar = setTimeout(() => {
+//   alert("Hola: ");
+// }, 5000);
+
+// const spam = setInterval(() => {
+//   console.log("Spam");
+// }, 1000);
